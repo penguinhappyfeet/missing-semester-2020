13b3fbf64bea4eee505c9bd031573c9f4e9f4faa
penguinhappyfeet <chierh.cheng@gmail.com>
2023-10-19 11:20:10 +0100

    comments added

--- lecture2/marco.sh
+++ lecture2/marco.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
-
+# Define the marco function
 marco(){
+    # Set the MARCO_DIR environment variable to the current directory
     export MARCO_DIR=$(pwd)
 }
 
+# Define the polo function
 polo(){
+    # Change the current directory to the value of the MARCO_DIR environment variable
     cd $MARCO_DIR
 }
